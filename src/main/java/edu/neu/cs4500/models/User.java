3785f242d563445459858ab83d99796776461b0b
Vincent Carlino <vincentcarlino@Vincents-MacBook-Pro.local>
2019-01-14 12:16:46 -0500

    Add User model class

---
+++ src/main/java/edu/neu/cs4500/models/User.java
@@ -0,0 +1,44 @@
+package edu.neu.cs4500.models;
+
+public class User {
+	private Integer id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+
+
+}
