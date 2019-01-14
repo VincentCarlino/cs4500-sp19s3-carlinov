ac2920384a6f3933b2856ef2c3b52c7d876c32f7
Vincent Carlino <vincentcarlino@Vincents-MacBook-Pro.local>
2019-01-14 13:03:18 -0500

    Add all users endpoint for UserService controller

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -1,5 +1,27 @@
 package edu.neu.cs4500.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.neu.cs4500.models.User;
+
+@RestController
 public class UserService {
+	static List<User> users = new ArrayList();
+	
+	static {
+		users.add(new User(123, "alice", "alice", "Alice", "Wonderland"));
+		users.add(new User(234, "bob", "bob", "Bob", "Marley"));
+	}
+	
+	@RequestMapping("/api/user")
+	public List<User> findAllUsers() {
+		return users;
+	}
+
+
 
 }
